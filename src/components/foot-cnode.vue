3ce3406b5a37d68d17216f2b73583f886c730459
zanetti4 <intermilan4@aliyun.com>
2018-12-02 19:20:58 +0800

    将页脚的样式改为less。

--- src/components/foot-cnode.vue
+++ src/components/foot-cnode.vue
@@ -29,7 +29,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .foot {
   background-color: white;
   font-size: 14px;
@@ -39,9 +39,9 @@ export default {
   padding: 20px;
   padding-right: 0;
   line-height: 2;
+  div {font-size: 12px;}
+  a:hover {color: #08C;}
 }
-.foot-col div {font-size: 12px;}
-.foot-col a:hover {color: #08C;}
 .foot-col-logo {
   display: inline-block;
   width: 95px;
